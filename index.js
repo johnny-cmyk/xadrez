78765af8da14f9519ab2560f34d6b1df7d5a2430
jonathan.gonzalez <125562481+johnny-cmyk@users.noreply.github.com>
2024-11-27 08:34:33 -0300

    alterações

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 class Tabuleiro {
     constructor() {
-        this.nome = "Tabuleiro;"
+        this.nome = "Tabuleiro";
 
         this.tabuleiro = [];
-        for (let linha=0; linha < 8; linha++) {
+        for (let linha = 0; linha < 8; linha++) {
             this.tabuleiro[linha] = [];
             for (let coluna = 0; coluna < 8; coluna++) {
                 this.tabuleiro[linha][coluna] = new Casa(linha, coluna);
@@ -11,11 +11,17 @@ class Tabuleiro {
         }
         console.log(this.tabuleiro);
     }
-}
 
-colocarPeça(peça,linha,coluna)  
-    const casa = this.tabuleiro[linha][coluna]
-    casa.elementoHtml.innerHTML = peca.simbolo;
+    colocarPeca(peca, linha, coluna) {
+        const casa = this.tabuleiro[linha][coluna];
+        casa.peca = peca;
+        casa.elementoHtml.innerHTML = peca.simbolo;
+    }
+
+    clicarCasa(casa) {
+        console.log(casa);
+    }
+}
 
 class Casa {
     constructor(linha, coluna) {
@@ -26,13 +32,16 @@ class Casa {
         this.elementoHtml = document.createElement('div');
         this.elementoHtml.classList.add('casa');
 
-        if((linha + coluna) % 2 == 0) {
-            this.elementoHtml.classList.add('clara')
+        if ((linha + coluna) % 2 == 0) {
+            this.elementoHtml.classList.add('clara');
         } else {
-            this.elementoHtml.classList.add('escura')
+            this.elementoHtml.classList.add('escura');
         }
         document.getElementById('tabuleiro').appendChild(this.elementoHtml);
-        console.log("foi");
+
+        this.elementoHtml.addEventListener('click', () => {
+            tabuleiro.clicarCasa(this);
+        })
     }
 }
 
@@ -54,30 +63,69 @@ class Peca {
     }
 }
 
-
 class Peao extends Peca {
     constructor(cor, linha, coluna) {
-        super(cor, linha,coluna);
-        this.simbolo = cor === 'branca' ? '&#9823' : '&#9823';
+        super(cor, linha, coluna);
+        this.simbolo = cor === 'branca' ? '&#9817;' : '&#9823;';
     }
 }
 
-peao = new Peao('branca', 1, 1);
-console.log(peao);
-const peca = new Tabuleiro();
-alert(tabuleiro.nome);
-tabuleiro.colocarPeca(peao,1,1);
-tabuleiro.colocarPeca(peao,1,2);
-tabuleiro.colocarPeca(peao,1,3);
-tabuleiro.colocarPeca(peao,1,4);
-tabuleiro.colocarPeca(peao,1,5);
-tabuleiro.colocarPeca(peao,1,6);
-tabuleiro.colocarPeca(peao,1,7);
-tabuleiro.colocarPeca(peao,1,8);
+class Torre extends Peca {
+    constructor(cor, linha, coluna) {
+        super(cor, linha, coluna);
+        this.simbolo = cor === 'branca' ? '&#9814;' : '&#9820;';
+    }
+}
 
+class Cavalo extends Peca {
+    constructor(cor, linha, coluna) {
+        super(cor, linha, coluna);
+        this.simbolo = cor === 'branca' ? '&#9816;' : '&#9822;';
+    }
+}
 
+class Bispo extends Peca {
+    constructor(cor, linha, coluna) {
+        super(cor, linha, coluna);
+        this.simbolo = cor === 'branca' ? '&#9815;' : '&#9821;';
+    }
+}
 
+class Rei extends Peca {
+    constructor(cor, linha, coluna) {
+        super(cor, linha, coluna);
+        this.simbolo = cor === 'branca' ? '&#9812;' : '&#9818;';
+    }
+}
 
+class Rainha extends Peca {
+    constructor(cor, linha, coluna) {
+        super(cor, linha, coluna);
+        this.simbolo = cor === 'branca' ? '&#9813;' : '&#9819;';
+    }
+}
 
+const tabuleiro = new Tabuleiro();
 
+// Peças Brancas
+const pecasBrancas = [
+    new Peao('branca', 1, 0), new Peao('branca', 1, 1), new Peao('branca', 1, 2), new Peao('branca', 1, 3),
+    new Peao('branca', 1, 4), new Peao('branca', 1, 5), new Peao('branca', 1, 6), new Peao('branca', 1, 7),
+    new Torre('branca', 0, 0), new Torre('branca', 0, 7), new Cavalo('branca', 0, 1), new Cavalo('branca', 0, 6),
+    new Bispo('branca', 0, 2), new Bispo('branca', 0, 5), new Rainha('branca', 0, 3), new Rei('branca', 0, 4)
+];
 
+peao1 = new Peao('preta', 6, 0);
+tabuleiro.colocarPeca(peao1,6,0);
+
+// Peças Pretas
+const pecasPretas = [
+    new Peao('preta', 6, 0), new Peao('preta', 6, 1), new Peao('preta', 6, 2), new Peao('preta', 6, 3),
+    new Peao('preta', 6, 4), new Peao('preta', 6, 5), new Peao('preta', 6, 6), new Peao('preta', 6, 7),
+    new Torre('preta', 7, 0), new Torre('preta', 7, 7), new Cavalo('preta', 7, 1), new Cavalo('preta', 7, 6),
+    new Bispo('preta', 7, 2), new Bispo('preta', 7, 5), new Rainha('preta', 7, 3), new Rei('preta', 7, 4)
+];
+
+// Colocar as peças brancas e pretas no tabuleiro
+pecasBrancas.forEach(peca => tabuleiro.colocarPeca(peca, peca.linha, peca.coluna));
+pecasPretas.forEach(peca => tabuleiro.colocarPeca(peca, peca.linha, peca.coluna));
